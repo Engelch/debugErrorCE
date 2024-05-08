a5012f70a8fc8d44f4049d31c5e86066170ea467
Christian ENGEL <engel-ch@outlook.com>
2024-05-08 13:49:00 +0200

    debugerrorce:v3.7.0:psql routines integrated

--- v3/version.go
+++ v3/version.go
@@ -1,7 +1,10 @@
 package debugerrorce
 
+// 3.7 psql helpers
+// 3.6 String2Md5
+// 3.5 CurrentFunctionName limits the function name output
 // 3.4 IP helpers
 
-const Debugerrorce_app_version = "3.6.0"
+const Debugerrorce_app_version = "3.7.0"
 
 // EOF
